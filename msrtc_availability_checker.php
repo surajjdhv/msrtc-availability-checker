2bd511942e663831ab2fba07467b6d6dec4982e4
Suraj Jadhav <mail@surajjadhav.me>
2021-08-28 22:20:35 +0530

    Use SOAP open API

--- msrtc_availability_checker.php
+++ msrtc_availability_checker.php
@@ -1,9 +1,7 @@
 <?php
 
 use GuzzleHttp\Client;
-use PHPHtmlParser\Dom;
 use React\EventLoop\Loop;
-use GuzzleHttp\Cookie\CookieJar;
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -12,12 +10,6 @@ Dotenv\Dotenv::createImmutable(__DIR__)->safeLoad();
 
 // Setup http client
 $http = $client = new Client();
-
-$cookies = CookieJar::fromArray([
-	'PHPSESSID' => $_ENV['SESSION_ID']
-], 'public.msrtcors.com');
-
-$dom = new Dom;
 
 function notify($title, $message)
 {
@@ -34,48 +26,65 @@ function notify($title, $message)
 	return $response->getStatusCode() == 200;
 }
 
+$xml = <<<XML
+<v:Envelope
+	xmlns:i="http://www.w3.org/2001/XMLSchema-instance"
+	xmlns:d="http://www.w3.org/2001/XMLSchema"
+	xmlns:c="http://schemas.xmlsoap.org/soap/encoding/"
+	xmlns:v="http://schemas.xmlsoap.org/soap/envelope/">
+	<v:Header />
+	<v:Body>
+		<n0:getSeatAvailability id="o0" c:root="1"
+			xmlns:n0="http://service.msrtc.trimax.com/">
+			<SeatAvailabilityRequest i:type="d:anyType">
+				<serviceId i:type="d:int">{$_ENV['SERVICE_ID']}</serviceId>
+				<fromStopId i:type="d:string">{$_ENV['FROM_STOP']}</fromStopId>
+				<toStopId i:type="d:string">{$_ENV['TO_STOP']}</toStopId>
+				<dateOfJourney i:type="d:string">{$_ENV['DATE_OF_JOURNEY']}</dateOfJourney>
+			</SeatAvailabilityRequest>
+		</n0:getSeatAvailability>
+	</v:Body>
+</v:Envelope>
+XML;
+
 // Run loop every 5 minutes after finishing the callback
-Loop::addPeriodicTimer(1, function ($timer) use ($http, $cookies, $dom) {
+Loop::addPeriodicTimer(1, function ($timer) use ($http, $xml) {
 	echo "Sending query ... \n";
 
-	$response = $http->get('https://public.msrtcors.com/ticket_booking/availability_lookup.php', [
-		'query' => [
-			'time' => $_ENV['BUS_TIME'],
-			'bsn' => $_ENV['BSN_NO']
+	$response = $http->post('http://api.msrtcors.com:8083/MSRTCMobileServices/MSRTCMobileService?wsdl', [
+		'headers' => [
+			'Content-Type' => 'text/xml; charset=UTF8',
 		],
-		'cookies' => $cookies
+		'body' => $xml,
 	]);
 
 	echo "Processing response ... \n";
 
-	$allSeats = $dom->loadStr((string) $response->getBody())->find('#seat_layout td')->toArray();
+	$responseXml = (array) simplexml_load_string((string) $response->getBody())
+		->children('S', true)
+		->children('ns3', true)
+		->children('', true)
+		->SeatAvailabilityResponse;
 
-	if (count($allSeats)) {
-		$availableSeats = array_filter($allSeats, function ($seat) {
-			return str_contains($seat->outerHtml, 'background-color:#00FFFF');
-		});
-	
-		if (count($availableSeats)) {
-			echo "Seats available ... \n";
-	
-			$availableSeatNumbers = array_map(function ($seat) {
-				return trim($seat->find('td')->innerHtml);
-			}, $availableSeats);
+	if (array_key_exists('availableSeats', $responseXml)) {
 		
-			sort($availableSeatNumbers);
-	
-			echo "Sending notification ... \n";
+		echo "Seats available ... \n";
 		
-			notify('Seats available', 'Seats are available for ' . $_ENV['BUS_TIME'] . ' with seat numbers ' . implode(', ', $availableSeatNumbers));
+		$availableSeatNumbers = (array) $responseXml['availableSeats']->availableSeat;
 	
-			echo "Stopping process ... \n";
-			
-			Loop::cancelTimer($timer);
-		} else {
-			echo "No seats available, will check back in 5 minutes ... \n";
+		sort($availableSeatNumbers);
 
-			error_log('Last ran: ' . date("m/d/Y h:i:s a", time()) . PHP_EOL, 3, __DIR__ . '/logs.txt');
-		}
+		echo "Sending notification ... \n";
+	
+		notify('Seats available', 'Seats are available for ' . $_ENV['DATE_OF_JOURNEY'] . ' with seat numbers ' . implode(', ', $availableSeatNumbers));
+
+		echo "Stopping process ... \n";
+		
+		Loop::cancelTimer($timer);
+	} else if (array_key_exists('serviceError', $responseXml) && $responseXml['serviceError']->errorReason == "No seat available !") {
+		echo "No seats available, will check back in 5 minutes ... \n";
+
+		error_log('Last ran: ' . date("m/d/Y h:i:s a", time()) . PHP_EOL, 3, __DIR__ . '/logs.txt');
 	} else {
 		echo "Invalid response! Notifying ... \n";
 		
